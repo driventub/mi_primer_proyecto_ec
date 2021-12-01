39c0985c7b259cd5b3d3a728ac65f25f9a7d44a3
bedel <bedel@DESKTOP-J3L2V9I.attlocal.net>
2021-11-30 18:54:41 -0600

    Cambio 1

---
+++ src/main/java/mi/primer/proyecto/pa/Animal.java
@@ -0,0 +1,13 @@
+package mi.primer.proyecto.pa;
+
+public class Animal {
+
+	public static void main(String[] args) {
+		System.out.println("Cambio 1");
+//		System.out.println("Cambio 2");
+//		System.out.println("Cambio 3");
+		
+
+	}
+
+}
---
+++ src/main/java/mi/primer/proyecto/pa/PruebaMaven.java
@@ -0,0 +1,10 @@
+package mi.primer.proyecto.pa;
+
+public class PruebaMaven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
