c1cacd9fc688977845453977ebe2119239106b4c
driventub <aldazpaulo@gmail.com>
2021-12-07 20:26:22 -0500

    Clase de inyeccion

---
+++ src/main/java/com/ec/edu/inyecction/dependencias/extendida/EstudianteArquitectura.java
@@ -0,0 +1,23 @@
+package com.ec.edu.inyecction.dependencias.extendida;
+
+public class EstudianteArquitectura extends Estudiante{
+	
+	public void pagarServiPagos(String nombre) {
+		System.out.println(nombre + "El estudiante hace el pago");
+		System.out.println(nombre + "El estudiante presenta el pago odonto ");
+		System.out.println("efectivo");
+		
+	}
+	
+	private int idMunicipio;
+
+	public int getIdMunicipio() {
+		return idMunicipio;
+	}
+
+	public void setIdMunicipio(int idMunicipio) {
+		this.idMunicipio = idMunicipio;
+	}
+	
+	
+}
