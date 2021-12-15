9d45bda6848fcbb280970fc63c7ef88a79427a75
USUARIO <USUARIO@DESKTOP-NJCV4OK>
2021-12-14 20:36:01 -0500

    Traller ID trabajo colaborativo

--- src/main/java/nicolas/carrera/Principal.java
+++ src/main/java/nicolas/carrera/Principal.java
@@ -7,10 +7,12 @@ public class Principal {
 
 	public static void main(String[] args) {
 		ApplicationContext app = new ClassPathXmlApplicationContext("com/uce/edu/archivo/beans.xml");
-		
+
+		// Estudiante
 		Estudiante est = (Estudiante)app.getBean("estudiante");
 		System.out.println("Nombre estudiante: " + est.getNombre() + " Apellido: " + est.getApellido() + " Edad: " + est.getEdad());
 		
+		// Profesor
 		Profesor prf = (Profesor)app.getBean("profesor");
 		System.out.println("Nombre profesor: " + prf.getNombre() + " Apellido: " + prf.getApellido() + " Profesion: " + prf.getProfesion() + " Universidad: " + prf.getUnivarsidad());
 	}
